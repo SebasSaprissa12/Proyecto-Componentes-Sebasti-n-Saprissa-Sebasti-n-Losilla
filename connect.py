567c23c01be00a33802f370d82aee6609f63543d
Sebastián Saprissa <ssaprissaa@ucenfotec.ac.cr>
2023-04-05 14:11:01 -0700

    Se añade carpeta templates

--- connect.py
+++ connect.py
@@ -1,41 +1,23 @@
-import os 
+from flask import Flask, render_template
 from google.cloud import bigquery
-from jinja2 import Template
+import os
+
+app = Flask(__name__)
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'clouddemo-service-account.json.json'
-
 cliente = bigquery.Client()
 
-sql_query = """
-SELECT nombre, id, clasificacion, precio, fecha_de_salida, stock
-FROM `sanguine-orb-379207.Juegos.Games` LIMIT 1000
-"""
+@app.route('/')
+def index():
+    sql_query = """
+    SELECT nombre, id, clasificacion, precio, fecha_de_salida, stock
+    FROM `sanguine-orb-379207.Juegos.Games` LIMIT 1000
+    """
 
-query_job = cliente.query(sql_query)
+    query_job = cliente.query(sql_query)
+    rows = [dict(row) for row in query_job.result()]
 
-# Crea una plantilla HTML utilizando jinja2
-template = Template('''
-<html>
-    <head>
-        <title>Resultado de la consulta de BigQuery</title>
-    </head>
-    <body>
-        <table>
-            {% for row in rows %}
-            <tr>
-                {% for value in row.values() %}
-                <td>{{ value }}</td>
-                {% endfor %}
-            </tr>
-            {% endfor %}
-        </table>
-    </body>
-</html>
-''')
+    return render_template('index.html', rows=rows)
 
-# Genera el resultado HTML utilizando la plantilla y los datos de la consulta de BigQuery
-result_html = template.render(rows=query_job.result())
-
-# Escribe el resultado en un archivo HTML
-with open('resultado.html', 'w') as f:
-    f.write(result_html)
+if __name__ == '__main__':
+    app.run(debug=True)
